6e55a0151eaf43d3c85c1ef900fdd70a51fdfc01
Ted Hess <thess@kitschensync.net>
2020-11-12 11:30:02 -0500

    Remove unused shell script

--- ports/apollo3/undef-list.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-make BOARD=artemis_thing_plus clean > /dev/null
-make BOARD=artemis_thing_plus V=1 2>&1 | grep "undefined\ reference" | tee unref.lst > /dev/null
-cat unref.lst | awk '{split($0,ary,"`"); gsub("\047", "",ary[2]); print ary[2]}' | sort | uniq -c | tee unref_todo.txt
